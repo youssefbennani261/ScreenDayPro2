8cf6c49ad059606e9fa1d76b07b448e81dc942a3
simo <simo@gmail.com>
2020-02-13 18:53:56 +0100

    dd

--- js/main.js
+++ js/main.js
@@ -34,11 +34,6 @@ if(window.location.pathname=="/ScreenDayPro2/index.php"){
 
     })
 }
-<<<<<<< HEAD
-=======
- 
-
->>>>>>> 4a49301de76b698cd9214ee9048822dcf6d48675
 // pour calculer pourcetage de difference entre ce mois et le dernier mois
 function increasedecrease(thismonth,lastmonth){
     var output="";
