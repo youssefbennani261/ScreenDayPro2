72d5da62af56835f444d89a886b898e0c6018d2f
simo <simo@gmail.com>
2020-02-20 22:42:32 +0100

    update page agence

--- php/listagence.php
+++ php/listagence.php
@@ -2,9 +2,9 @@
 require("connection.php");
 session_start();
          $id=$_SESSION['riad'][0];
-      $req=mysqli_query($con,"select a.Num_agence, a.Nom,a.Directeur,a.Adresse,a.Telephone from agence a join demande d on a.Num_agence=d.Num_agence join reservation r on r.Num_Demande =d.Num_Demande  and d.Num_Riad=".$id." group by a.Num_agence ") or die ("!!!!!!!!!!!!!!!!!!");
+      $req=mysqli_query($con,"select a.Num_agence, a.Nom,a.Directeur,a.Adresse,a.Telephone,Logo_src from agence a join demande d on a.Num_agence=d.Num_agence join reservation r on r.Num_Demande =d.Num_Demande  and d.Num_Riad=".$id." group by a.Num_agence ") or die ("!!!!!!!!!!!!!!!!!!");
  while($row=mysqli_fetch_array($req))
-     $tab[]=array("num"=>$row[0],"nom"=>$row[1],"directeur"=>$row[2],"adresse"=>$row[3],"tel"=>$row[4]);
+     $tab[]=array("num"=>$row[0],"nom"=>$row[1],"directeur"=>$row[2],"adresse"=>$row[3],"tel"=>$row[4],"logo"=>$row[5]);
  
      echo json_encode($tab);
 ?>
--- php/upload.php
+++ php/upload.php
@@ -1,38 +1,4 @@
 <?php
-
-  require('connection.php');
-  session_start(); 
-  if(isset($_POST['submit'])){
-
-    function saveimage($name,$image,$con,$id){
-        $req=mysqli_query($con,"insert into imagepro values(NULL,'".$name."','".$image."',".$id.")") or die  ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-         $req1=mysqli_query($con,"select image from imagepro where id_riad=".$id."") or die("!!!!!!!!!!!!!!!!!!");
-         while($row=mysqli_fetch_array($req1)){
-          $_SESSION['imgprofile']=$row[0];
-         }
-          
-
-    }
-  
-  if(getimagesize($_FILES['file']['tmp_name'])==false)
-  {
-      echo 'please select un images';
-  }
- else
-   {
-       $image=addslashes($_FILES['file']['tmp_name']);
-       $name=addslashes($_FILES['file']['name']);
-        $image=file_get_contents($image);
-        $image=base64_encode($image);
-        saveimage($name,$image,$con,$_SESSION["riad"][0]);
-        
-        
-
-   }
-
-
-  }
-
 if($_FILES["file"]["name"] != '')
 {
 require("connection.php");
